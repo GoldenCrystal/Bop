57ae3e80f59ea97aa66d6588b860fbc141c1a367
GoldenCrystal <8518235+GoldenCrystal@users.noreply.github.com>
2022-01-04 21:44:23 +0100

    🎨 Improve iTunes process detection.

--- Bop/ITunesProcessHelper.cs
+++ Bop/ITunesProcessHelper.cs
@@ -1,12 +1,40 @@
-﻿using System.Buffers;
+using System.Buffers;
 using System.Runtime.InteropServices;
+using Microsoft.Win32;
 using Microsoft.Win32.SafeHandles;
 using static Bop.Interop;
 using static Bop.Interop.Kernel32;
 
 namespace Bop;
+
 internal static class ITunesProcessHelper
 {
+	/// <summary>Locates the iTunes executable according to Windows registry.</summary>
+	/// <remarks>This method hasn't been tested on non-store iTunes.</remarks>
+	/// <param name="path">The path to iTunes, if found.</param>
+	/// <returns><see langword="true"/> if the path to iTunes.exe was found; otherwise <see langword="false"/>.</returns>
+	public static bool TryLocateITunesBinary(out string? path)
+	{
+		using (var key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\iTunes.exe", false))
+		{
+			if (key is not null)
+			{
+				path = key.GetValue(null) as string;
+				if (path is not null)
+				{
+					return true;
+				}
+			}
+		}
+
+		path = null;
+		return false;
+	}
+
+	// Refresh the ITunes path after at least 10 minutes have ellapsed. (This is mainly useful for when the path is not found)
+	private const long PathRefreshDelayInMinutes = 10;
+	private static Tuple<string?, DateTime> _lastKnownITunesPath = Tuple.Create(null as string, DateTime.UnixEpoch);
+
 	/// <summary>Wait for an active iTunes.exe process.</summary>
 	/// <remarks>
 	/// This method uses polling to find an iTunes process, with the caveats mentioned for <see cref="FindITunesProcess"/>.
@@ -23,7 +51,7 @@ internal static class ITunesProcessHelper
 		var pool = ArrayPool<int>.Shared;
 		var array = pool.Rent(expectedProcessCountUpperBound); // This could be not enough, but there doesn't seem to be a very good way to detect this.
 
-		const int maxDelay = 60_000;
+		const int MaxDelay = 60_000;
 		int delay = 1_000;
 
 		try
@@ -34,9 +62,9 @@ internal static class ITunesProcessHelper
 
 				await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
 
-				if (delay < maxDelay)
+				if (delay < MaxDelay)
 				{
-					delay = Math.Min(delay * 2, maxDelay);
+					delay = Math.Min(delay * 2, MaxDelay);
 				}
 			}
 		}
@@ -136,7 +164,7 @@ internal static class ITunesProcessHelper
 			return null;
 		}
 
-		return span[..length].EndsWith(@"\iTunes.exe");
+		return IsITunesProcessPath(span[..length]);
 	}
 
 	private static bool IsITunesProcessArrayPool(SafeProcessHandle process)
@@ -161,8 +189,50 @@ internal static class ITunesProcessHelper
 			array = pool.Rent(requestedLength);
 		}
 
-		bool result = array.AsSpan(0, length).EndsWith(@"\iTunes.exe");
+		bool result = IsITunesProcessPath(array.AsSpan(0, length));
 		pool.Return(array);
 		return result;
+	}
+
+	/// <summary>Identify a process that is or could be iTunes.</summary>
+	/// <remarks>
+	/// The conditions for a match are as follows:
+	/// <list type="bullet">
+	/// <description>The process name must be <c>iTunes.exe</c>.</description>
+	/// <description>If the path to iTunes was found in the registry, there must be an exact match.</description>
+	/// <description>If the path to iTunes was not found in the registry, or the path doesn't match, the cached path to iTunes is refreshed on a 10 min delay.</description>
+	/// <description>If the path to iTunes was not found in the registry despite an up-to date cache, any iTunes.exe process will match.</description>
+	/// </list>
+	/// </remarks>
+	/// <param name="path">The path to check.</param>
+	/// <returns></returns>
+	public static bool IsITunesProcessPath(Span<char> path)
+	{
+		if (path.EndsWith(@"\iTunes.exe"))
+		{
+			var (lastKnownPath, dateTime) = _lastKnownITunesPath;
+
+			if (lastKnownPath is not null && path.SequenceEqual(lastKnownPath))
+			{
+				return true;
+			}
+
+			var now = DateTime.UtcNow;
+
+			// Refresh the cached path every 10 minutes.
+			if ((now - dateTime).Ticks > 10 * TimeSpan.TicksPerMinute)
+			{
+				TryLocateITunesBinary(out lastKnownPath);
+				Volatile.Write(ref _lastKnownITunesPath, Tuple.Create(lastKnownPath, now));
+
+				return lastKnownPath is null || path.SequenceEqual(lastKnownPath);
+			}
+			else if (lastKnownPath is null)
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
